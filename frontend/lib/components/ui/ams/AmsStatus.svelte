79308afbb7d88b8e6f4d29ee793ae644a9ba66ea
Aman <17457975+achhabra2@users.noreply.github.com>
2025-02-17 11:17:21 -0500

    Add ping indicator when AMS or External Spool Filament is Active

--- frontend/lib/components/ui/ams/AmsStatus.svelte
+++ frontend/lib/components/ui/ams/AmsStatus.svelte
@@ -73,11 +73,17 @@
                     <Eye />
                   </div>
                 {:else}
-                  <div class="text-shadow flex flex-col items-center">
+                  <div class="flex flex-col items-center">
                     <span class="text-shadow text-md">?</span>
                   </div>
                 {/if}
               </div>
+              {#if extSpool.active}
+                <span class="relative flex size-3">
+                    <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+                    <span class="relative inline-flex size-3 rounded-full bg-green-400"></span>
+                </span>
+              {/if}
             </div>
           </div>
         </div>
@@ -154,19 +160,16 @@
                                     </Popover.Portal>
                                 </Popover.Root>
                             {:else}
-                                <span class="text-slate-400">?</span>
+                                <div class="flex flex-col items-center">
+                                    <span class="text-slate-400">?</span>
+                                </div>
                             {/if}
                         </div>
                         {#if slot.active}
-                            <div class="absolute -bottom-10 flex w-full flex-col items-center">
-                                <div class="h-5 w-0.5 bg-slate-600"></div>
-                                <div class="-mt-px flex h-5 w-5 items-center justify-center rounded-full bg-slate-700">
-                                    <div
-                                        class="h-2.5 w-2.5 rounded-full"
-                                        style="background-color: {formatColor(slot.color)}"
-                                    ></div>
-                                </div>
-                            </div>
+                            <span class="relative flex size-3">
+                                <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+                                <span class="relative inline-flex size-3 rounded-full bg-green-400"></span>
+                            </span>
                         {/if}
                     </div>
                 {/each}
