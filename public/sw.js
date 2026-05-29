a79d541a1a7135f0eb3b84e247e7227966fd486c
KevDoy <KevDoy@users.noreply.github.com>
2026-05-29 13:48:20 -0700

    Simplify service worker and remove unused elements
    Remove 'template' service worker caching as it is not used in the application

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,3 @@
-const CACHE_NAME = "bambui-v1";
-
-self.addEventListener("install", (event) => {
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-    event.waitUntil(clients.claim());
-});
-
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
-    );
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (event) => event.waitUntil(clients.claim()));
+self.addEventListener("fetch", () => {});
